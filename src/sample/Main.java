e715e3997304562f975801c00bcc7b92b8132a66
Tommaso Patriti <ro0trexustom@gmail.com>
2021-06-02 10:11:41 +0200

    Update Main.java
    gestione del la scena per renderla scalabile

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,7 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.beans.binding.Bindings;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -11,8 +12,18 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 800, 800));
+
+        primaryStage.setMinHeight(750);
+        primaryStage.setMinWidth(750);
+
+        root.scaleXProperty().bind(Bindings.min(primaryStage.widthProperty().divide(primaryStage.minWidthProperty()),
+                primaryStage.heightProperty().divide(primaryStage.minHeightProperty())));
+
+        root.scaleYProperty().bind(root.scaleXProperty());
+
         primaryStage.show();
     }
 
